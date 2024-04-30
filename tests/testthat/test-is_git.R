a5a60f458c64dcda0882f613b98043557229ceac
Jeffrey W. Hollister <jeff.w.hollister@gmail.com>
2024-04-30 14:43:31 -0400

    testing inside of testing too much...  Added test for is_git

---
+++ tests/testthat/test-is_git.R
@@ -0,0 +1,3 @@
+test_that("is_git works", {
+  expect_type(epar::is_git(), "logical")
+})
--- tests/testthat/test-test_and_badge.R
+++
@@ -1,3 +0,0 @@
-test_that("onedrive mirror works", {
-  expect_type(epar::one_drive_mirror(), "character")
-})
