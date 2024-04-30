7e7d9d6f538134e079cfa53133602d4544cd483e
Jeffrey W. Hollister <jeff.w.hollister@gmail.com>
2024-04-30 07:28:20 -0400

    added function to test a package and create a test coverage badge.

---
+++ R/test_and_badge.R
@@ -0,0 +1,21 @@
+#' Run package tests and create badge
+#' 
+#' This function uses the \code{covr} package to run tests for a package and 
+#' returns a markdown badge code reporting the code coverate that can be used on
+#' a README (or any markdown).
+#' 
+#' @param package path to package
+#' 
+#' @export
+test_and_badge <- function(package = "."){
+  pkg_cov <- covr::package_coverage(package)
+  cov_perc <- round(covr::percent_coverage(pkg_cov), 1)
+  color_rmp <- colorRampPalette(c("red", "orange", "yellow","darkgreen"))(10)
+  color_rmp <- gsub("#", "", color_rmp)
+  indx <- ifelse(round(cov_perc/10) == 0, 1, round(cov_perc/10))
+  my_color <- color_rmp[indx]
+  badge <- badger::badge_custom("test coverage", paste0(cov_perc, "%"), 
+                                my_color)
+  cat(badge)
+}
+
